9df75d90dbc0c48d116dde8bc7aaa43989f7954a
Anastasia Smirnov <a.kurdeka@gmail.com>
2022-04-28 15:29:56 -0400

    task_7 added new method fill_contact form

--- fixture/contact.py
+++ fixture/contact.py
@@ -12,39 +12,19 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_name("submit").click()
 
-    def fillin_add_contact_form(self, contact):
+    def add(self, contact):
         # fill in add contact form
         wd = self.app.wd
         self.click_add_contact()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("152")
-
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
+        self.fill_in_contact_form(contact)
         self.submit_add_contact_form()
         self.return_to_contact_page()
 
-    def fillin_edit_contact_form(self, contact):
-        # fill in add contact form
+
+    def fill_in_contact_form(self, contact):
+
         wd = self.app.wd
-        wd.find_element_by_xpath("//tbody/tr/td[8]").click()
+
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -67,6 +47,17 @@ class ContactHelper:
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(contact.email)
+
+
+
+    def edit(self, contact):
+        # fill in add contact form
+        wd = self.app.wd
+
+        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_xpath("//tbody/tr/td[8]").click()
+
+        self.fill_in_contact_form(contact)
 
         wd.find_element_by_name("update").click()
         self.return_to_contact_page()
@@ -82,6 +73,7 @@ class ContactHelper:
 
     def delete(self):
         wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//div/input[@value ='Delete']").click()
 
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,7 +6,7 @@ from model.contact import Contact
 def test_add_contact(app):
         app.contact.open_home_page()
         app.session.login( username="admin", password=  "secret")
-        app.contact.fillin_add_contact_form(Contact(firstname= f"{datetime.datetime.now().strftime('%M')}name",
+        app.contact.add(Contact(firstname= f"{datetime.datetime.now().strftime('%M')}name",
                                             lastname =f"{datetime.datetime.now().strftime('%M')}lastname",
                                             address=f"{datetime.datetime.now().strftime('%M')}address",
                                             home_phone=f"{datetime.datetime.now().strftime('%M%S')}",
