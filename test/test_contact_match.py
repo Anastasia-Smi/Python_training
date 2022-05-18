f39a663edc76d190ccac2ea51350785d83dfd1b4
Anastasia Smirnov <a.kurdeka@gmail.com>
2022-05-18 13:37:09 -0400

    task_14

--- test/test_contact_match.py
+++ test/test_contact_match.py
@@ -5,8 +5,9 @@ def test_contacts_on_home_page(app):
     contact_from_edit_page= app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 
@@ -16,7 +17,7 @@ def clear(s):
 
 
 def clear_email(s):
-    return re.sub("[)'(,-]","",s)
+    return re.sub("[)'(,]","",s)
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
@@ -25,9 +26,15 @@ def merge_phones_like_on_home_page(contact):
                                        [contact.home_phone,contact.mobile_phone,
                                         contact.work_phone,]))))
 
+#def merge_emails_like_on_home_page(contact):
+    #return "\n".join(filter(lambda x: x != "",
+                            #map(lambda x: clear_email(str(x)),
+                                #filter(lambda x: x is not None,
+                                       #[contact.e_mail, contact.e_mail_2,
+                                       #contact.e_mail_3]))))
+
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear_email(str(x)),
-                                filter(lambda x: x is not None,
-                                       [contact.e_mail, contact.e_mail_2,
+    return "\n".join(map(str,filter(lambda x: x != "",
+                                    map(lambda x: clear_email(str(x)),
+                             [contact.e_mail, contact.e_mail_2,
                                        contact.e_mail_3]))))
